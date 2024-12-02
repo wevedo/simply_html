8bdf109d944e0c57cdb410a1d547e370fd26f11d
ibrahim adams <abutech782@gmail.com>
2024-12-02 15:48:29 +0300

    Create url2.js

---
+++ scs/url2.js
@@ -0,0 +1,70 @@
+const { adams } = require("../Ibrahim/adams");
+const { Catbox } = require("node-catbox");
+const fs = require('fs-extra');
+const { downloadAndSaveMediaMessage } = require('@whiskeysockets/baileys');
+
+// Initialize Catbox
+const catbox = new Catbox();
+
+// Function to upload a file to Catbox and return the URL
+async function uploadToCatbox(filePath) {
+  if (!fs.existsSync(filePath)) {
+    throw new Error("File does not exist");
+  }
+  try {
+    const uploadResult = await catbox.uploadFile({ path: filePath });
+    if (uploadResult) {
+      return uploadResult;
+    } else {
+      throw new Error("Error retrieving file link");
+    }
+  } catch (error) {
+    throw new Error(String(error));
+  }
+}
+
+// Command to upload image, video, or audio file
+adams({
+  'nomCom': 'url2',       // Command to trigger the function
+  'categorie': "General", // Command category
+  'reaction': '👨🏿‍💻'    // Reaction to use on command
+}, async (groupId, client, context) => {
+  const { msgRepondu, repondre } = context;
+
+  // If no message (image/video/audio) is mentioned, prompt user
+  if (!msgRepondu) {
+    return repondre("Please mention an image, video, or audio.");
+  }
+
+  let mediaPath;
+
+  // Check if the message contains a video
+  if (msgRepondu.videoMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.videoMessage);
+  }
+  // Check if the message contains an image
+  else if (msgRepondu.imageMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.imageMessage);
+  }
+  // Check if the message contains an audio file
+  else if (msgRepondu.audioMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.audioMessage);
+  } else {
+    // If no media (image, video, or audio) is found, prompt user
+    return repondre("Please mention an image, video, or audio.");
+  }
+
+  try {
+    // Upload the media to Catbox and get the URL
+    const fileUrl = await uploadToCatbox(mediaPath);
+
+    // Delete the local media file after upload
+    fs.unlinkSync(mediaPath);
+
+    // Respond with the URL of the uploaded file
+    repondre(fileUrl);
+  } catch (error) {
+    console.error("Error while creating your URL:", error);
+    repondre("Oops, there was an error.");
+  }
+});
