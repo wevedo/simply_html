e8aea81943f7c7d7a9f1fd594a842941f2604ccd
ibrahim adams <abutech782@gmail.com>
2024-12-25 10:19:37 +0300

    Update aaadula.js

--- scs/aaadula.js
+++ scs/aaadula.js
@@ -39,7 +39,7 @@ const fetchGitHubStats = async () => {
     }
 };
 
-adams({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+adams({ nomCom: "dula", categorie: "General" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre, prefixe, nomAuteurMessage } = commandeOptions;
     let { cm } = require(__dirname + "/../Ibrahim/adams");
     var coms = {};
