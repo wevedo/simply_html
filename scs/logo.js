a666925f7e7b8833fb2b531224961ec63386f91a
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2025-01-05 10:23:23 +0300

    Update logo.js

--- scs/logo.js
+++ scs/logo.js
@@ -21,48 +21,6 @@ const logoCommands = [
     makerUrl: "https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html"
   },
   {
-    nomCom: "marvel",
-    categorie: "Logo",
-    reaction: "📱",
-    makerUrl: "https://ephoto360.com/tao-logo-phong-cach-marvel-419.html"
-  },
-  {
-    nomCom: "wall",
-    categorie: "Logo",
-    reaction: "👍",
-    makerUrl: "https://textpro.me/break-wall-text-effect-871.html"
-  },
-  {
-    nomCom: "summer",
-    categorie: "Logo",
-    reaction: "🌞",
-    makerUrl: "https://textpro.me/create-sunset-light-text-effects-online-for-free-1124.html"
-  },
-  {
-    nomCom: "neonlight",
-    categorie: "Logo",
-    reaction: "💡",
-    makerUrl: "https://textpro.me/create-glowing-neon-light-text-effect-online-free-1061.html"
-  },
-  {
-    nomCom: "greenneon",
-    categorie: "Logo",
-    reaction: "🟢",
-    makerUrl: "https://textpro.me/green-neon-text-effect-874.html"
-  },
-  {
-    nomCom: "glitch",
-    categorie: "Logo",
-    reaction: "🎛️",
-    makerUrl: "https://textpro.me/create-impressive-glitch-text-effects-online-1027.html"
-  },
-  {
-    nomCom: "spiderman",
-    categorie: "Logo",
-    reaction: "😈",
-    makerUrl: "https://ephoto360.com/tao-banner-phong-cach-spider-man-465.html"
-  },
-  {
     nomCom: "boom",
     categorie: "Logo",
     reaction: "💥",
@@ -73,36 +31,6 @@ const logoCommands = [
     categorie: "Logo",
     reaction: "💦",
     makerUrl: "https://en.ephoto360.com/create-water-effect-text-online-295.html"
-  },
-  {
-    nomCom: "galaxy",
-    categorie: "Logo",
-    reaction: "❄️",
-    makerUrl: "https://ephoto360.com/tao-hinh-nen-dien-thoai-galaxy-theo-ten-dep-full-hd-684.html"
-  },
-  {
-    nomCom: "transformer",
-    categorie: "Logo",
-    reaction: "🤖",
-    makerUrl: "https://textpro.me/create-a-transformer-text-effect-online-1035.html"
-  },
-  {
-    nomCom: "thunder",
-    categorie: "Logo",
-    reaction: "⚡",
-    makerUrl: "https://textpro.me/online-thunder-text-effect-generator-1031.html"
-  },
-  {
-    nomCom: "harrypotter",
-    categorie: "Logo",
-    reaction: "🧙‍♂️",
-    makerUrl: "https://textpro.me/create-harry-potter-text-effect-online-1025.html"
-  },
-  {
-    nomCom: "cat",
-    categorie: "Logo",
-    reaction: "🪟",
-    makerUrl: "https://textpro.me/write-text-on-foggy-window-online-free-1015.html"
   },
   {
     nomCom: "whitegold",
