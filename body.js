9038312108016ca2cc18508cc360450d19eb88ef
ibrahim adams <abutech782@gmail.com>
2025-01-16 20:13:15 +0300

    Update body.js

--- body.js
+++ body.js
@@ -28,6 +28,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const baileys_1 = __importStar(require("@whiskeysockets/baileys"));
 const logger_1 = __importDefault(require("@whiskeysockets/baileys/lib/Utils/logger"));
+const { isJidGroup } = require('@whiskeysockets/baileys');
 const logger = logger_1.default.child({});
 logger.level = 'silent';
 const pino = require("pino");
