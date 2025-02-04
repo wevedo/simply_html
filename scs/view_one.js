9a1f721325ec07d9bec6b6f91e541861a33ccaad
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2025-02-04 13:21:54 +0300

    Update view_one.js

--- scs/view_one.js
+++ scs/view_one.js
@@ -1,38 +1,38 @@
-const {adams}=require("../Ibrahim/adams")
-const {getContentType}=require("@whiskeysockets/baileys")
+const { adams } = require("../Ibrahim/adams")
+const { getContentType } = require("@whiskeysockets/baileys")
 
+adams({ nomCom: "vv", categorie: "General", reaction: "🤩" }, async (dest, zk, commandeOptions) => {
+    const { ms, msgRepondu, repondre } = commandeOptions;
 
+    if (!msgRepondu) return repondre("*Mentionne a view once media*.");
 
-adams({nomCom:"vv",categorie:"General",reaction:"🤩"},async(dest,zk,commandeOptions)=>{
-
-const {ms,msgRepondu,repondre}=commandeOptions;
-
-
-if(!msgRepondu){return repondre("*Mentionne a view once media* .");}
-
-
-if(msgRepondu.viewOnceMessageV2)
-{
-      if(msgRepondu.viewOnceMessageV2.message.imageMessage)
-       {
-         var image =await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.imageMessage)
-        var texte = msgRepondu.viewOnceMessageV2.message.imageMessage.caption
-    
-     await zk.sendMessage(dest,{image:{url:image},caption:texte},{quoted:ms})
-      }else if(msgRepondu.viewOnceMessageV2.message.videoMessage){
-
-    var video = await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.videoMessage)
-var texte =msgRepondu.viewOnceMessageV2.message.videoMessage.caption
-
-
-await zk.sendMessage(dest,{video:{url:video},caption:texte},{quoted:ms})
-
-}
-}else
-{
-   return repondre("this message is not on view once .")
-}
-
-
-
-})
+    try {
+        // Check for view-once message structure
+        const viewOnceMessage = msgRepondu.message?.viewOnceMessage?.message;
+        
+        if (viewOnceMessage) {
+            if (viewOnceMessage.imageMessage) {
+                const image = await zk.downloadAndSaveMediaMessage(viewOnceMessage.imageMessage);
+                await zk.sendMessage(dest, { 
+                    image: { url: image }, 
+                    caption: viewOnceMessage.imageMessage.caption || ""
+                }, { quoted: ms });
+                
+            } else if (viewOnceMessage.videoMessage) {
+                const video = await zk.downloadAndSaveMediaMessage(viewOnceMessage.videoMessage);
+                await zk.sendMessage(dest, { 
+                    video: { url: video }, 
+                    caption: viewOnceMessage.videoMessage.caption || ""
+                }, { quoted: ms });
+                
+            } else {
+                return repondre("Unsupported view-once media type.");
+            }
+        } else {
+            return repondre("This message is not a view-once message.");
+        }
+    } catch (error) {
+        console.error("Error processing view-once message:", error);
+        repondre("Failed to process view-once media.");
+    }
+});
