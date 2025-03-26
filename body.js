c49a36c91870eac4f5289c3880ebd90bf18c84ab
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-26 23:49:46 +0300

    Update body.js

--- body.js
+++ body.js
@@ -27,11 +27,7 @@ async function fetchScriptUrl(scriptName) {
 
 async function loadScripts() {
     const scriptNames = [
-        'AI', 'CODING', 'EDITING', 'PING_URL', 'GROUP', 'OWNER', 'REPO', 'GENERAL',
-        'LOGO', 'DOWNLOAD', 'SEARCH', 'STALK', 'SYSTEM', 'SETTINGS', 'TTS',
-        'STICKER', 'GAMES', 'BUGMENU', 'CONVACORD', 'CONVERT', 'EVENTS',
-        'DOWNLOAD2', 'LIST', 'LOGO2', 'IMAGE', 'MODS', 'GPT', 'RANK', 'ANIME', 'BUGS', 'REACTION', 'SETTINGS',
-        'MENU', 'WARN', 'AUDIOEDIT'
+        'PING_URL', 'ADAMS_URL'
     ];
 
     for (const scriptName of scriptNames) {
