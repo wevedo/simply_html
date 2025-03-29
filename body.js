22ed5582ce7c732345321d3159d8950bf07a7d74
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-29 15:58:06 +0300

    Update body.js

--- body.js
+++ body.js
@@ -48,7 +48,7 @@ app.listen(PORT, () => console.log(`Bwm xmd is starting with a speed of ${PORT}m
 
 //============================================================================//
 // In your main bot file
-const { getSettings } = require('./Ibrahim/adams');
+const { getSettings } = require('./utils/settings');
 
 // Modified message handler
 adams.ev.on("messages.upsert", async ({ messages }) => {
