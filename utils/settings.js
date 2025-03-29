c4235959dfce1a2966a8c125415ab6a863730199
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-29 15:38:49 +0300

    Create settings.js

---
+++ utils/settings.js
@@ -0,0 +1,25 @@
+const fs = require('fs-extra');
+const path = require('path');
+const conf = require('/../config');
+
+const SETTINGS_PATH = path.join(__dirname, '..', 'bwm-settings.json');
+
+async function getSettings() {
+    try {
+        return await fs.readJson(SETTINGS_PATH);
+    } catch (e) {
+        return { 
+            AUTO_BIO: false,
+            ANTICALL: false 
+        };
+    }
+}
+
+async function updateFeature(feature, state) {
+    const settings = await getSettings();
+    settings[feature] = state === 'on';
+    await fs.writeJson(SETTINGS_PATH, settings);
+    return settings;
+}
+
+module.exports = { getSettings, updateFeature };
