d40c49e4aa41841c3bdf1a60ee25e49044f98e91
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-29 23:37:51 +0300

    Update body.js

--- body.js
+++ body.js
@@ -370,22 +370,6 @@ adams.ev.on("messages.upsert", async ({ messages }) => {
     await updatePresence(adams, msg.key.remoteJid);
 });
 
-// Create messageUtils.js if not existing
-// File: messageUtils.js
-// module.exports = {
-//     getMessageContent: (message) => {
-//         try {
-//             const type = Object.keys(message)[0];
-//             switch(type) {
-//                 case 'conversation': return message.conversation;
-//                 case 'extendedTextMessage': return message.extendedTextMessage.text;
-//                 case 'imageMessage': return message.imageMessage?.caption;
-//                 case 'videoMessage': return message.videoMessage?.caption;
-//                 default: return '';
-//             }
-//         } catch (e) { return ''; }
-//     }
-// };
 
 //===============================================================================================================//
 
