2d5e6e952ac9eb29ba9adc257e8a4c4b02b18d93
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-31 00:57:30 +0300

    .

--- Ibrahim/adams.js
+++ Ibrahim/adams.js
@@ -6,7 +6,7 @@ function adams(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🚘";
+        infoComs.reaction = "🧿";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
