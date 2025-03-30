b76081a498cedd26ee06a2cd5408782f5f3b17ee
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-30 13:01:26 +0300

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -3,51 +3,28 @@
 const NEWS_LETTER_JID = "120363285388090068@newsletter";
 const BOT_NAME = "BWM-XMD";
 const DEFAULT_THUMBNAIL = "https://files.catbox.moe/sd49da.jpg";
-const GITHUB_AUDIO_BASE = "https://raw.githubusercontent.com/ibrahimaitech/bwm-xmd-music/master/tiktokmusic";
-const AUDIO_FILES_COUNT = 161;
 
-const NEWS_LETTER_CONTEXT = {
-    newsletterJid: NEWS_LETTER_JID,
-    newsletterName: BOT_NAME,
-    serverMessageId: () => Math.floor(100000 + Math.random() * 900000)
-};
-
-const getRandomAudio = () => ({
-    url: `${GITHUB_AUDIO_BASE}/sound${Math.floor(Math.random() * AUDIO_FILES_COUNT) + 1}.mp3`,
-    mimetype: "audio/mpeg",
-    ptt: true
+const createContext = (userJid, options = {}) => ({
+    contextInfo: {
+        mentionedJid: [userJid],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: NEWS_LETTER_JID,
+            newsletterName: BOT_NAME,
+            serverMessageId: Math.floor(100000 + Math.random() * 900000)
+        },
+        externalAdReply: {
+            title: options.title || BOT_NAME,
+            body: options.body || "Premium WhatsApp Bot Solution",
+            thumbnailUrl: options.thumbnail || DEFAULT_THUMBNAIL,
+            mediaType: 1,
+            showAdAttribution: true,
+            renderLargerThumbnail: false
+        }
+    }
 });
 
-const createContext = (userJid, options = {}) => {
-    // Validate and format JID
-    const formattedJid = userJid?.endsWith('@s.whatsapp.net') 
-        ? userJid 
-        : `${(userJid || '').replace(/\D/g, "")}@s.whatsapp.net`;
-
-    return {
-        contextInfo: {
-            mentionedJid: [formattedJid],
-            forwardingScore: 999,
-            isForwarded: true,
-            forwardedNewsletterMessageInfo: {
-                ...NEWS_LETTER_CONTEXT,
-                serverMessageId: NEWS_LETTER_CONTEXT.serverMessageId()
-            },
-            externalAdReply: {
-                title: options.title || BOT_NAME,
-                body: options.body || "Premium WhatsApp Bot Solution",
-                thumbnailUrl: options.thumbnail || DEFAULT_THUMBNAIL,
-                mediaType: 1,
-                showAdAttribution: true,
-                renderLargerThumbnail: false
-            }
-        }
-    };
-};
-
 module.exports = {
-    getRandomAudio,
-    createContext,
-    DEFAULT_THUMBNAIL,
-    BOT_NAME
+    createContext
 };
