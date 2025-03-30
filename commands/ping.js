76df42a944e8fd13fbeaa98adc8a509922157924
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-30 13:03:09 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 // commands/ping.js
-const { createContext } = require("../utils/contextManager");
+const { createContext } = require("../utils/helper");
 const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
