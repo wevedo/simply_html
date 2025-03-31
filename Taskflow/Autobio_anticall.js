07f31be050b3f7684c457ec6ff8c80d17f68d240
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-31 18:27:42 +0300

    .

--- Taskflow/Autobio_anticall.js
+++ Taskflow/Autobio_anticall.js
@@ -1,14 +1,6 @@
-const fs = require("fs");
-const path = require("path");
 const { adams } = require("../Ibrahim/adams");
+const systemManager = require("../bwmxmd/Autobio_anticall");
 
-const configPath = path.join(__dirname, "../Session/store.json");
-
-// Function to load and save settings
-const loadConfig = () => JSON.parse(fs.readFileSync(configPath, "utf8"));
-const saveConfig = (config) => fs.writeFileSync(configPath, JSON.stringify(config, null, 2), "utf8");
-
-// Command: Toggle Auto Bio
 adams(
   { nomCom: "autobio", reaction: "ℹ️", nomFichier: __filename },
   async (dest, zk, { arg, repondre }) => {
@@ -16,15 +8,11 @@ adams(
       return repondre("⚠️ Use: /autobio on or /autobio off");
     }
 
-    let config = loadConfig();
-    config.AUTO_BIO = arg[0];
-    saveConfig(config);
-
+    systemManager.updateSetting("AUTO_BIO", arg[0]);
     repondre(`✅ Auto Bio has been turned *${arg[0]}*`);
   }
 );
 
-// Command: Toggle Anti-Call
 adams(
   { nomCom: "anticall", reaction: "📵", nomFichier: __filename },
   async (dest, zk, { arg, repondre }) => {
@@ -32,10 +20,7 @@ adams(
       return repondre("⚠️ Use: /anticall on or /anticall off");
     }
 
-    let config = loadConfig();
-    config.ANTICALL = arg[0];
-    saveConfig(config);
-
+    systemManager.updateSetting("ANTICALL", arg[0]);
     repondre(`✅ Anti-Call has been turned *${arg[0]}*`);
   }
 );
