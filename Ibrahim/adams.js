80aaafcef128fbeb8b0ede01bb1dbc895a4de7be
Ibrahim Adams <mribrahimadams1@gmail.com>
2025-03-31 12:13:52 +0300

    Update adams.js

--- Ibrahim/adams.js
+++ Ibrahim/adams.js
@@ -1,16 +1,16 @@
-var tabCmds = [];
+const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+
+const tabCmds = [];
 let cm = [];
+const ev = {}; 
+
 function adams(obj, fonctions) {
     let infoComs = obj;
-    if (!obj.categorie) {
-        infoComs.categorie = "General";
-    }
-    if (!obj.reaction) {
-        infoComs.reaction = "🧿";
-    }
+    if (!obj.categorie) infoComs.categorie = "General";
+    if (!obj.reaction) infoComs.reaction = "🚘";
     infoComs.fonction = fonctions;
     cm.push(infoComs);
-    // console.log('chargement...')
     return infoComs;
 }
-module.exports = { adams, Module: adams, cm };
+
+module.exports = { adams, Module: adams, cm, ev };
