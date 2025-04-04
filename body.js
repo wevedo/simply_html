3c7c202cb613d2d982b61a0631f598cca970f9bb
wevedo <mribraahtech@gmail.com>
2025-04-04 18:53:20 +0300

    Update body.js

--- body.js
+++ body.js
@@ -67,8 +67,6 @@ app.use(express.static("public"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 app.listen(PORT, () => console.log(`Bwm xmd is starting on port ${PORT}`));
 
-const zlib = require('zlib');
-
 async function authentification() {
     try {
         if (!fs.existsSync(__dirname + "/Session/creds.json")) {
