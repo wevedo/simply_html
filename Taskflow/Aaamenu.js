71b21b6c754b7027fa572bb338cab08b4d27a829
wevedo <mribraahtech@gmail.com>
2025-04-05 14:48:29 +0300

    Update Aaamenu.js

--- Taskflow/Aaamenu.js
+++ Taskflow/Aaamenu.js
@@ -127,7 +127,7 @@ adams({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions
                         text: "© Powered By BWM-XMD"
                     }),
                     header: proto.Message.InteractiveMessage.Header.create({
-                        ...(await prepareWAMessageMedia({ image: fs.readFileSync('https://bwm-xmd-files.vercel.app/bwmxmd_1tksj5.jpeg') }, { upload: zk.waUploadToServer })),
+                        ...(await prepareWAMessageMedia({ image: fs.readFileSync('./IMG-20250405-WA0025.jpg') }, { upload: zk.waUploadToServer })),
                         title: ``,
                         gifPlayback: true,
                         subtitle: "",
