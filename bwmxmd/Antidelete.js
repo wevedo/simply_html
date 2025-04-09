ab7a6dbd8fc023990d55fe52cada232fa2d3b13c
wevedo <mribraahtech@gmail.com>
2025-04-09 09:08:29 +0300

    Update Antidelete.js

--- bwmxmd/Antidelete.js
+++ bwmxmd/Antidelete.js
@@ -1,5 +1,5 @@
 const { downloadMediaMessage } = require('@whiskeysockets/baileys');
-const { createContext } = require('../utils/helper');
+const { createContext } = require('../Ibrahim/helper');
 const fs = require('fs');
 const path = require('path');
 const stream = require('stream');
